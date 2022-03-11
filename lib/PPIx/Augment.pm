f90edd424af1b75e41b2979a75bcc21bbaa03d8b
Branislav Zahradník <barney@cpan.org>
2022-03-11 13:22:31 +0100

    Setup - Dist::Zilla, .gitignore, .github/workflows

---
+++ lib/PPIx/Augment.pm
@@ -0,0 +1,31 @@
+
+use v5.14;
+use warnings;
+
+package PPIx::Augment {
+	1;
+}
+
+__END__
+
+=pod
+
+=encoding utf-8
+
+=head1 NAME
+
+PPIx::Augment - Improve structure of PPI document
+
+=head1 DESCRIPTION
+
+Fix some bugs and improve structre of PPI documents
+
+=head1 AUTHOR
+
+Branislav Zahradník <barney@cpan.org>
+
+=head1 COPYRIGHT AND LICENCE
+
+Test::YAFT distribution is distributed under Artistic Licence 2.0.
+
+=cut
