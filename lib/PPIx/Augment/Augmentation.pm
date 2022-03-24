3d7817e897e6eaa9d8b4e91c7f7f7bb87cb63071
Branislav Zahradník <barney@cpan.org>
2022-03-24 08:23:55 +0100

    PPIx::Augmentation - base class

---
+++ lib/PPIx/Augment/Augmentation.pm
@@ -0,0 +1,36 @@
+
+use v5.14;
+use warnings;
+
+package PPIx::Augment::Augmentation {
+
+	sub requires {
+	}
+
+	sub augment {
+	}
+
+	1;
+}
+
+__END__
+
+=pod
+
+=encoding utf-8
+
+=head1 NAME
+
+PPIx::Augment::Augmentation - Base class for augmentations
+
+=head1 DESCRIPTION
+
+=head1 AUTHOR
+
+Branislav Zahradník <barney@cpan.org>
+
+=head1 COPYRIGHT AND LICENCE
+
+This file is part of L<PPIx::Augment> distribution.
+
+=cut
